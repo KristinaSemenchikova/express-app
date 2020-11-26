03bf23d0a93ee0c4b41716030d16cf2820fb9035
KristinaSemenchikova <kristinaalekseevna333@gmail.com>
2020-11-26 13:08:12 +0300

    Small fix

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const { PORT = 3000 } = process.env;
 const startServer = async () => {
   try {
     app.listen(PORT, () => {
-      console.log(`Server running on PORT ${PORT} :)`);
+      console.log(`Server running on PORT ${PORT}`);
     });
   } catch (e) {
     console.log(e);
